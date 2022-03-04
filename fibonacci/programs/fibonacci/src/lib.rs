de8496ce4ae8c9db6fc7d20bfb7e81fa893b239e
Paul Linville <plinville12@gmail.com>
2022-03-04 14:42:40 -0700

    Creating a simple counter program w/ mutable Counter account

--- fibonacci/programs/fibonacci/src/lib.rs
+++ fibonacci/programs/fibonacci/src/lib.rs
@@ -1,15 +1,43 @@
 use anchor_lang::prelude::*;
 
-declare_id!("AzhpMgr3apBd5K3WzLMWZudHjhMPoxfH6qACrRNUPBvp");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod fibonacci {
+mod fibonacci {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, start: u64) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = *ctx.accounts.authority.key;
+        counter.count = start;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = authority, space = 48)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
--- fibonacci/tests/fibonacci.ts
+++ fibonacci/tests/fibonacci.ts
@@ -1,17 +1,66 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Fibonacci } from "../target/types/fibonacci";
+
+import * as anchor from '@project-serum/anchor';
+
+const assert = require("assert");
+const { SystemProgram } = anchor.web3;
 
 describe("fibonacci", () => {
+  const provider = anchor.Provider.local();
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.Fibonacci as Program<Fibonacci>;
-  console.log(program);
+  // Counter for the tests.
+  const counter = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  // Program for the tests.
+  const program = anchor.workspace.Fibonacci;
+
+  it("Creates a counter", async () => {
+    await program.rpc.initialize(new anchor.BN(0), {
+      accounts: {
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [counter],
+    });
+
+    let counterAccount = await program.account.counter.fetch(counter.publicKey);
+
+    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));
+    assert.ok(counterAccount.count.toNumber() === 0);
+  });
+
+  it("Updates a counter", async () => {
+    await program.rpc.increment({
+      accounts: {
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+      },
+    });
+
+    const counterAccount = await program.account.counter.fetch(
+      counter.publicKey
+    );
+
+    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));
+    assert.ok(counterAccount.count.toNumber() == 1);
+  });
+
+  it("Updates a counter again", async () => {
+    await program.rpc.increment({
+      accounts: {
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+      },
+    });
+
+    const counterAccount = await program.account.counter.fetch(
+      counter.publicKey
+    );
+
+    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));
+    assert.ok(counterAccount.count.toNumber() == 2);
   });
 });
